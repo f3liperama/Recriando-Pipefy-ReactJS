684f387946cd468cec3e388233d5bc39f18adf22
f3liperama <f3liperama@gmail.com>
2019-11-13 17:29:19 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Header() {
+    return(
+        <Container>
+            <h1>Hodefy</h1>
+        </Container>
+    );
+}
